382fc035c95cc0514b5065079c595f0790ca57e4
Alin Babu <alinbabu2010@gmail.com>
2023-03-07 17:24:08 +0530

    Add change notifier provider and change app title

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:top_news/provider/news_provider.dart';
 import 'package:top_news/screens/list_screen.dart';
+import 'package:top_news/utils/constants.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -11,12 +14,16 @@ class NewsApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Top News',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => NewsProvider(),
+      child: MaterialApp(
+        title: Constants.appTitle,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const NewsListScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const NewsListScreen(),
     );
   }
 }
